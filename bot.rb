358528dc6da44b884bfcf0113c08b3f7bad4007d
William Mathewson <s1321070@sms.ed.ac.uk>
2014-11-24 16:19:24 +0000

    Use new BotConfig module

--- bot.rb
+++ bot.rb
@@ -1,16 +1,17 @@
 require 'cinch'
 require 'redis'
+
 require_relative 'config'
 require_relative 'plugins/reader'
 
-config = BotConfig.new
 $redis = Redis.new
+
 bot = Cinch::Bot.new do
   configure do |c|
-    puts config::server
-    c.server = config::server
-    c.channels = config::channels.map{ |x| "#" + x}
-    c.plugins.prefix = config::prefix
+    puts BotConfig::BASE_DIR
+    c.server = BotConfig::SERVER
+    c.channels = BotConfig::CHANNELS.map{ |x| "#" + x}
+    c.plugins.prefix = BotConfig::PREFIX
     c.plugins.plugins = [Reader, Saver, Getter]
   end
 end
